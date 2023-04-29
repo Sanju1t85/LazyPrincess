6f2f476a68329c278a020b03d77ebad0848c3c59
Sanju1t85 <101995551+Sanju1t85@users.noreply.github.com>
2023-04-29 11:24:51 +0530

    Update info.py

--- info.py
+++ info.py
@@ -67,7 +67,7 @@ AI = is_enabled((environ.get("AI","True")), False)
 OPENAI_API = environ.get("OPENAI_API","")
 LAZY_AI_LOGS = int(environ.get("LAZY_AI_LOGS","")) #GIVE YOUR NEW LOG CHANNEL ID TO STORE MESSAGES THAT THEY SEARCH IN BOT PM.... [ i have added this to keep an eye on the users message, to avoid misuse of LazyPrincess ]
 # Requested Content template variables ---
-ADMIN_USRNM = environ.get('ADMIN_USRNM','real_heros') # WITHOUT @
+ADMIN_USRNM = environ.get('ADMIN_USRNM','sksbackup1') # WITHOUT @
 MAIN_CHANNEL_USRNM = environ.get('MAIN_CHANNEL_USRNM','sksbackup1') # WITHOUT @
 DEV_CHANNEL_USRNM = environ.get('DEV_CHANNEL_USRNM','sksdev') # WITHOUT @
 LAZY_YT_HANDLE = environ.get('LAZY_YT_HANDLE','telegramwallah3835')  # WITHOUT @ [  add only handle - don't add full url  ] 
